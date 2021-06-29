70cb821aad93997479904775da7bdab6f66f9b7e
StevenQ01 <84299911+StevenQ01@users.noreply.github.com>
2021-06-29 01:09:22 +0000

    removed capitals

--- candidate-testing.js
+++ candidate-testing.js
@@ -6,7 +6,7 @@ let candidateName = (" ");
 let question = ("Who was the first American woman in space? ");
 let correctAnswer = "Sally Ride";
 let candidateAnswer;
-let questions = ["Who was the first American woman in space? ", "True or False: 5 Kilometer == 5000meters? ", "(5+3)/2*10 = ?", "Given the array [8, 'orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "] ;
+let questions = ["Who was the first American woman in space? ", "True or False: 5 kilometer == 5000meters? ", "(5+3)/2*10 = ?", "Given the array [8, 'orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "] ;
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 //et quizAnswers = [];
