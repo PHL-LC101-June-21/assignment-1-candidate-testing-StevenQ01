6a5d77410f5ffeb2cb120f5c0b84d111389ceea0
StevenQ01 <84299911+StevenQ01@users.noreply.github.com>
2021-06-29 01:05:55 +0000

    removed capitalization

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,10 +3,10 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = (" ");
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = ("Who was the first American Woman in space? ");
+let question = ("Who was the first American woman in space? ");
 let correctAnswer = "Sally Ride";
 let candidateAnswer;
-let questions = ["Who was the first American Woman in space? ", "True or False: 5 Kilometer == 5000meters? ", "(5+3)/2*10 = ?", "Given the array [8, 'orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "] ;
+let questions = ["Who was the first American woman in space? ", "True or False: 5 Kilometer == 5000meters? ", "(5+3)/2*10 = ?", "Given the array [8, 'orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "] ;
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 //et quizAnswers = [];
